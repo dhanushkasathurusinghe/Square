abd5611bd40eea9a9b82570d47e85f266106276d
SHARUKH <sunillocal37@gmail.com>
2019-09-08 15:54:10 +0530

    test

---
+++ app/src/main/java/com/example/appoinmentdetails/ViewAppoinment.kt
@@ -0,0 +1,12 @@
+package com.example.appoinmentdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewAppoinment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_appoinment)
+    }
+}
